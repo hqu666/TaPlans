0f05c65d4d1e113f63d321c1aeb59585d6dba078
hqu666@gmail.com <hqu666@gmail.com>
2024-06-10 22:35:04 +0900

    #2 対象予定への遷移

--- app/src/main/java/com/hijiyam_koubou/taplans/MainActivity.java
+++ app/src/main/java/com/hijiyam_koubou/taplans/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_arget_plan, R.id.nav_other_setting)
+                R.id.nav_home, R.id.nav_target_plan, R.id.nav_other_setting)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
